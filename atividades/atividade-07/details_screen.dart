a4c13e0e76e05546f411f470a5ba7b2fd8eb631c
lkayo14 <140459412+lkayo14@users.noreply.github.com>
2025-02-27 18:53:57 -0300

    Update details_screen.dart

--- atividades/atividade-07/details_screen.dart
+++ atividades/atividade-07/details_screen.dart
@@ -1,1 +1,34 @@
 
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  final Map<String, dynamic> crypto;
+
+  DetailsScreen({required this.crypto});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(crypto["nome"])),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Image.network(crypto["imagem"], width: 100, height: 100),
+            ),
+            SizedBox(height: 20),
+            Text("Nome: ${crypto["nome"]}",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            Text("Símbolo: ${crypto["simbolo"]}",
+                style: TextStyle(fontSize: 16)),
+            Text("Preço: \$${crypto["preco"]}", style: TextStyle(fontSize: 16)),
+            Text("Variação 24h: ${crypto["variacao_24h"]}%",
+                style: TextStyle(fontSize: 16)),
+          ],
+        ),
+      ),
+    );
+  }
+}
